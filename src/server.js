11e6d91e7645d6a5c205db0d078096ebb5d7ac07
Wellington Duarte <wduarte78@gmail.com>
2020-10-28 09:41:36 -0300

    Servidores/Modulos

---
+++ src/server.js
@@ -0,0 +1,4 @@
+var http = require('http');//módulos js
+http.createServer(function(req, res){//criar servidor
+    res.end('DevPleno.com');//função anonima e assicrona
+}).listen(3000);// ouvir porta
